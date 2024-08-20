f7c6c5d2baab7f2e66c7d3185d58db61b70557ce
Maksad Khalipaev <maks14flay@gmail.com>
2024-08-20 19:57:03 +0500

    a-pay

--- src/js/index.js
+++ src/js/index.js
@@ -1,23 +1,8 @@
-import { languageSwitcher } from "./modules/languageSwitcher";
-import { callbackForm } from "./modules/callbackForm";
-import { inputMaskInit } from "./modules/inputMaskInit";
-import { servicesListDropDown } from "./modules/servicesListDropDown";
-import { menuListDropDown } from "./modules/menuListDropDown";
-import { mobileSlider } from "./modules/mobileSlider";
-import "./libs/input-mask.min";
-
-const onDocumentLoaded = () => {
-    const element = document.getElementById("curYear");
-
-    if (element) {
-        element.innerHTML = new Date().getFullYear();
-    }
-    setTimeout(servicesListDropDown, 300);
-    mobileSlider();
-    inputMaskInit()
-    languageSwitcher();
-    callbackForm();
-    menuListDropDown();
-};
-
-document.addEventListener("DOMContentLoaded", onDocumentLoaded);
+import "./libs/jquery.min.js";
+import "./libs/jquery-migrate.min";
+import "./libs/jquery.countdownplugin.min";
+import "./libs/jquery.countdown.min";
+import "./libs/bootstrap.bundle.min";
+import "./libs/iziToast.min";
+import "./libs/jquery.inputmask.min";
+import "./libs/payment.min";
